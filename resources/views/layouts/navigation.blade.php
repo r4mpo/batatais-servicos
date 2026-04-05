ad19b65c07b8ef40a8505f5be6d54a26c904afd2
r4mpo <erick1souza1ago04@gmail.com>
2026-04-05 01:05:06 -0300

    fix(): - Redirecionando corretamente;

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
             <ul class="navbar-nav ms-auto">
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#servicos">
+                    <a class="nav-link" href="{{ route('home') }}#servicos">
                         {{ __('labels.services') }}
                     </a>
                 </li>
